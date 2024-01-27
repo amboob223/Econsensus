d7f5cbd56515125b7964be1e6a3280b91e8c8f4c
amboob223 <monk8896@gmail.com>
2024-01-27 09:38:36 -0500

    ww

--- src/App.js
+++ src/App.js
@@ -193,10 +193,15 @@ Our services are designed to empower your company, fostering innovation, efficie
 
 const Contacts = () => {
   return <><ul>
-        <li>Twitter</li>
-        <li>Linkedin</li>
-        <li>Tiktok</li>
-    </ul></>;
+        <li>Twitter<i class="fa-brands fa-instagram"></i></li>
+        <li>Linkedin<i class="fa-brands fa-linkedin"></i></li>
+        <li>Tiktok<i class="fa-brands fa-tiktok"></i></li>
+        <li>instagram<i class="fa-brands fa-instagram"></i></li>
+
+    </ul>
+    
+    </>;
+
 };
 
 //im about to make a component for the scorller that will show some stock phitos and they 
@@ -560,7 +565,11 @@ function App() {
       </Router>
       </header>
       <footer>
+        <div>
+          ©Econsensus 2024
+        </div>
         <Contacts/>
+
       </footer>
     
     </div>
