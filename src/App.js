55f027543fbfdd40e9d3a01e528a1f4fec4e9263
amboob223 <monk8896@gmail.com>
2024-02-04 05:09:36 -0500

    ddd

--- src/App.js
+++ src/App.js
@@ -13,32 +13,35 @@ import Workpage from "./components/Workpage";
 
 
 const Contacts = () => {
-  return <div>
-
-  <ul className="mog">
-      <div>
-          ©Econsensus 2024
-        </div>    
-        <a href="https://twitter.com/Econsensus1"><li>Twitter
-          <i className="fa-brands fa-instagram"></i></li>
+  return (
+    <div>
+      <ul className="mog">
+        <div>©Econsensus 2024</div>
+        <a href="https://twitter.com/Econsensus1">
+          <li>
+            Twitter <i className="fa-brands fa-instagram"></i>
+          </li>
         </a>
-        <li> <a   href="https://www.linkedin.com/in/amboob223/">
-         Linkedin<i className="fa-brands fa-linkedin"></i>
-     
-          </a></li>  
-          <a href="https://www.tiktok.com/@econsensus">
-             <li>Tiktok<i className="fa-brands fa-tiktok"></i></li>
-          </a>
-
-          <a href="https://www.instagram.com/econsensus1/">
-                  <li>instagram<i className="fa-brands fa-instagram"></i></li>
-          </a>
-     
-    </ul>
-    
-    </div>;
-
+        <a href="https://www.linkedin.com/in/amboob223/">
+          <li>
+            LinkedIn <i className="fa-brands fa-linkedin"></i>
+          </li>
+        </a>
+        <a href="https://www.tiktok.com/@econsensus">
+          <li>
+            Tiktok <i className="fa-brands fa-tiktok"></i>
+          </li>
+        </a>
+        <a href="https://www.instagram.com/econsensus1/">
+          <li>
+            Instagram <i className="fa-brands fa-instagram"></i>
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
 };
+
 
 const App = () => {
   return (
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,31 +2,33 @@ import React from "react";
 import "../App.css";
 
 const Contacts = () => {
-  return <div>
-
-  <ul className="mog">
-      <div>
-          ©Econsensus 2024
-        </div>    
-        <a href="https://twitter.com/Econsensus1"><li>Twitter
-          <i className="fa-brands fa-instagram"></i></li>
+  return (
+    <div>
+      <ul className="mog">
+        <div>©Econsensus 2024</div>
+        <a href="https://twitter.com/Econsensus1">
+          <li>
+            Twitter <i className="fa-brands fa-instagram"></i>
+          </li>
         </a>
-       <a> <li>    href="https://www.linkedin.com/in/amboob223/"
-         Linkedin<i className="fa-brands fa-linkedin"></i>
-     
-          </li> </a> 
-          <a href="https://www.tiktok.com/@econsensus">
-             <li>Tiktok<i className="fa-brands fa-tiktok"></i></li>
-          </a>
-
-          <a href="https://www.instagram.com/econsensus1/">
-                  <li>instagram<i className="fa-brands fa-instagram"></i></li>
-          </a>
-     
-    </ul>
-    
-    </div>;
-
+        <a href="https://www.linkedin.com/in/amboob223/">
+          <li>
+            LinkedIn <i className="fa-brands fa-linkedin"></i>
+          </li>
+        </a>
+        <a href="https://www.tiktok.com/@econsensus">
+          <li>
+            Tiktok <i className="fa-brands fa-tiktok"></i>
+          </li>
+        </a>
+        <a href="https://www.instagram.com/econsensus1/">
+          <li>
+            Instagram <i className="fa-brands fa-instagram"></i>
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
 };
 
 export default Contacts;
