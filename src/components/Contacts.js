b5a8c6348af5246e7a29ba8e26c5524495283159
amboob223 <monk8896@gmail.com>
2024-02-04 02:18:05 -0500

    ddd

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,31 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../App.css";
+
 const Contacts = () => {
-  return <div>
-
-  <ul className="mog">
-      <div>
-          ©Econsensus 2024
-        </div>    
-        <a href="https://twitter.com/Econsensus1"><li>Twitter
-          <i className="fa-brands fa-instagram"></i></li>
-        </a>
-        <li> <a   href="https://www.linkedin.com/in/amboob223/">
-         Linkedin<i className="fa-brands fa-linkedin"></i>
-     
-          </a></li>  
-          <a href="https://www.tiktok.com/@econsensus">
-             <li>Tiktok<i className="fa-brands fa-tiktok"></i></li>
-          </a>
-
-          <a href="https://www.instagram.com/econsensus1/">
-                  <li>instagram<i className="fa-brands fa-instagram"></i></li>
-          </a>
-     
-    </ul>
-    
-    </div>;
-
+  return (
+    <div>
+      <ul className="mog">
+        <div>©Econsensus 2024</div>
+        <li>
+          <Link to="https://twitter.com/Econsensus1">
+            Twitter
+            <i className="fa-brands fa-instagram"></i>
+          </Link>
+        </li>
+        <li>
+          <Link to="https://www.linkedin.com/in/amboob223/">
+            Linkedin
+            <i className="fa-brands fa-linkedin"></i>
+          </Link>
+        </li>
+        <li>
+          <Link to="https://www.tiktok.com/@econsensus">
+            Tiktok
+            <i className="fa-brands fa-tiktok"></i>
+          </Link>
+        </li>
+        <li>
+          <Link to="https://www.instagram.com/econsensus1/">
+            Instagram
+            <i className="fa-brands fa-instagram"></i>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Contacts;
