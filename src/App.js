c3892eae472ee25c35fa7106cc75445bbc35f07d
amboob223 <monk8896@gmail.com>
2024-02-04 19:12:35 -0500

    ddd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Help from "./components/Help";
 import Home from "./components/Home";
 import Blogpage from "./components/Blogpage";
 import Workpage from "./components/Workpage";
-
+import Blog from "./components/Blog";
 
 
 
@@ -53,6 +53,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/help" element={<Help />} />
+            <Route path ="/blog" element ={<Blog/>}/>
             <Route path="/blogpage" element={<Blogpage />} />
             <Route path="/workpage" element={<Workpage />} />
           </Routes>
