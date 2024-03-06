0261d85e096246fe1a8858b815d0c1a9326b4d36
amboob223 <monk8896@gmail.com>
2024-03-06 15:25:25 -0500

    render

--- server/server.js
+++ server/server.js
@@ -50,8 +50,8 @@ app.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: "http://localhost:3000/success.html",
-      cancel_url: "http://localhost:3000/cancel.html",
+      success_url: "http://dpg-cnkcck6d3nmc73e70qv0-a.oregon-postgres.render.com:3000/success.html",
+      cancel_url: "http://dpg-cnkcck6d3nmc73e70qv0-a.oregon-postgres.render.com:3000/cancel.html",
     });
 
     res.json({ id: session.id });
