6f9d0a839205be19800118d360bab5c1c30198d0
amboob223 <monk8896@gmail.com>
2024-03-06 17:32:33 -0500

    render

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const POOL = require("pg").Pool;
 const pool = new POOL({
-    user:"econsensus_user",
-    database:"econsensus",
-    port:5432,
-    password:"3kkOxuBGbc3AIMixgep2qlgWNqUQ6MF9",
-    host:"dpg-cnkcck6d3nmc73e70qv0-a.oregon-postgres.render.com"
+    connectionString: "postgres://econsensus_user:3kkOxuBGbc3AIMixgep2qlgWNqUQ6MF9@dpg-cnkcck6d3nmc73e70qv0-a.oregon-postgres.render.com/econsensus",
+     ssl: {
+        rejectUnauthorized: false
+    }
+
 })
 
 module.exports = pool;
