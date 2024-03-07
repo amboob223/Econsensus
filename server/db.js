1243831684d7703a03e685af0f89f102195d622a
amboob223 <monk8896@gmail.com>
2024-03-06 20:24:08 -0500

    dd

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const POOL = require("pg").Pool;
 const pool = new POOL({
     connectionString: "postgres://econsensus_user:3kkOxuBGbc3AIMixgep2qlgWNqUQ6MF9@dpg-cnkcck6d3nmc73e70qv0-a.oregon-postgres.render.com/econsensus",
-     ssl: {
+      ssl: {
         rejectUnauthorized: false
     }
 
