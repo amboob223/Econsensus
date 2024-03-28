745eae7f2d8157adde1d3365c10d8b0dc99d7598
amboob223 <monk8896@gmail.com>
2024-03-28 05:46:11 -0400

    nuke

--- src/components/ExampleCarouselImage.js
+++ src/components/ExampleCarouselImage.js
@@ -6,53 +6,64 @@ import img2 from "../images/bitcoin.jpg";
 import img3 from "../images/devfront.png";
 import { Link } from 'react-router-dom';
 
+
 function CarouselFadeExample() {
   return (
     <Carousel fade>
       <Carousel.Item>
         <Link to='/help'>
-          <img
-            className='pic'
-            src={img1}
-            alt="first"
-            width="300px" // Adjust width of image
-          />
+          <div style={{ position: 'relative' }}>
+            <img
+              className='pic'
+              src={img1}
+              alt="first"
+              width="300px" // Adjust width of image
+              style={{ display: 'block', margin: '0 auto' }} // Center the image horizontally
+            />
+            <div className="carousel-caption" >
+              <h3 className="caption-text">Econometrics</h3>
+              <p className="caption-text">Using data to make business better.</p>
+            </div>
+          </div>
         </Link>
-        <Carousel.Caption className="carousel-caption-bottom">
-          <h3 className="caption-text">Econometrics</h3>
-          <p className="caption-text"> Using data to make business better.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Link to='/help'>
-          <img
-            className='pic'
-            src={img2}
-            alt="two"
-            width="300px" // Adjust width of image
-          />
+          <div style={{ position: 'relative' }}>
+            <img
+              className='pic'
+              src={img2}
+              alt="two"
+              width="300px" // Adjust width of image
+              style={{ display: 'block', margin: '0 auto' }} // Center the image horizontally
+            />
+            <div className="carousel-caption">
+              <h3 className="caption-text">Blockchain</h3>
+              <p className="caption-text">Specializing in blockchain integration</p>
+            </div>
+          </div>
         </Link>
-        <Carousel.Caption className="carousel-caption-bottom">
-          <h3 className="caption-text">Blockchain</h3>
-          <p className="caption-text">Specializing in blockchain integration</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Link to='/help'>
-          <img
-            className='pic'
-            src={img3}
-            alt="third"
-            width="300px" // Adjust width of image
-          />
+          <div style={{ position: 'relative' }}>
+            <img
+              className='pic'
+              src={img3}
+              alt="third"
+              width="300px" // Adjust width of image
+              style={{ display: 'block', margin: '0 auto' }} // Center the image horizontally
+            />
+            <div className="carousel-caption" >
+              <h3 className="caption-text">Content Marketing</h3>
+              <p className="caption-text">We deliver content to connect messages to client needs</p>
+            </div>
+          </div>
         </Link>
-        <Carousel.Caption className="carousel-caption-bottom">
-  <h3 className="caption-text">Content Marketing</h3>
-  <p className="caption-text">Got an idea you want people to see? We can shoot all your content.</p>
-</Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
 }
+
 
 export default CarouselFadeExample;
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,27 +9,29 @@ import CarouselFadeExample from "./ExampleCarouselImage"
 
 const Home = () => {
   return (
-    <div  style={{"margin-top":"4%", "margin-bottom":"4%"}} className="home-content">
+    <div style={{ marginTop: "4%", marginBottom: "4%" }} className="home-content">
       <img src={econsensusLogo} alt="osh"/>
       <table>
-        <tr>
-          <td>
-            <p style={{ fontSize: '3em' }}>
-             Your in-house economist and software developer.
-            </p>
-             <p>
-              Specializing in Econometric services, blockchain-enhanced processes, and comprehensive web development, Econsensus is where innovation meets expertise.
-            </p>
-          </td>
-          <td>
-           
-            <CarouselFadeExample/>
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td style={{ width: "50%" }}>
+              <p style={{ fontSize: '3em' }}>
+                Your in-house economist and software developer.
+              </p>
+              <p>
+                Specializing in Econometric services, blockchain-enhanced processes, and comprehensive web development, Econsensus is where innovation meets expertise.
+              </p>
+            </td>
+            <td style={{ width: "50%", verticalAlign: "top" }}>
+              <CarouselFadeExample/>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
 };
+
 
 
 
