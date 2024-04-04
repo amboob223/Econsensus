fec65e52e1dd2f9884122945fa49df79f7470e7c
amboob223 <monk8896@gmail.com>
2024-04-04 18:58:00 -0400

    ddd

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,38 @@
-import {React} from "react";
-
-import econsensusLogo from '../images/Econsensus.png'; // Adjust the path
-import CarouselFadeExample from "./ExampleCarouselImage"
-
-
-
- 
+import React from "react";
+import econsensusLogo from '../images/Econsensus.png';
+import CarouselFadeExample from "./ExampleCarouselImage";
 
 const Home = () => {
   return (
-    <div style={{ marginTop: "4%", marginBottom: "4%" }} className="home-content">
-      <img src={econsensusLogo} alt="osh"/>
-      <table>
-        <tbody>
-          <tr>
-            <td style={{ width: "50%" }}>
-              <p style={{ fontSize: '3em' }}>
-                Your in-house economist and software developer.
-              </p>
-              <p>
-                Specializing in Econometric services, blockchain-enhanced processes, and comprehensive web development, Econsensus is where innovation meets expertise.
-              </p>
-            </td>
-            <td style={{ width: "50%", verticalAlign: "top" }}>
-              <CarouselFadeExample/>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="home-content">
+      <img src={econsensusLogo} alt="Econsensus Logo" className="logo" />
+      <div className="table-container">
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <div className="content">
+                  <p style={{ fontSize: '3em' }}>
+                    Your in-house economist and software developer.
+                  </p>
+                  <p>
+                    Specializing in Econometric services, blockchain-enhanced processes, and comprehensive web development, Econsensus is where innovation meets expertise.
+                  </p>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div className="carousel-container">
+                  <CarouselFadeExample />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
-
-
-
-
 
 export default Home;
