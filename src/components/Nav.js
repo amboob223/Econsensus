9af1512b5c4b8c975b2a583b277d73d75cba7cd3
amboob223 <monk8896@gmail.com>
2024-04-29 08:15:04 -0400

    dhuk

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,67 +1,87 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import econsensusLogo from "../images/Econsensus.png"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import econsensusLogo from "../images/Econsensus.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
-  return (<div >
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Set initial state to false
 
- 
-    <nav id="whoop"  style={{ marginRight: '10px', justifyContent:"space-between" }} className="navbar navbar-expand-lg navbar-light bg-light">
-     
-         <Link to="/">
-        <img src={econsensusLogo} alt="smasg" className="logo" />
-      </Link>
-     
-    
-     <div>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsMenuOpen(true); // Close the menu if screen size is small
+      } else {
+        setIsMenuOpen(false); // Open the menu if screen size is large
+      }
+    };
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">
-              About us
-            </Link>
-          </li>
-          <li className="nav-item dropdown">
-            <Link
-              className="nav-link dropdown-toggle"
-              
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-             OurWork
-            </Link>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <Link to="/workpage" className="dropdown-item">
-                Works
-              </Link>
-              <div className="dropdown-divider"></div>
-              <Link to="/blogpage" className="dropdown-item">
-                Blog
-              </Link>
-              
-            
-            </div>
-          </li>
-          <li className="nav-item">
-            <Link to="/help" className="nav-link">
-              How we can help
-            </Link>
-          </li>
-        </ul>
-      </div>
-       </div>
-    </nav> 
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div>
+      <nav
+        id="whoop"
+        style={{ marginRight: '10px', justifyContent: "space-between" }}
+        className="navbar navbar-expand-lg navbar-light bg-light"
+      >
+        <Link to="/">
+          <img src={econsensusLogo} alt="econsensus" className="logo" />
+        </Link>
+
+        {/* Show or hide the menu icon based on the state */}
+        {isMenuOpen ? null : (
+          <div  id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">
+                  About us
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Our Work
+                </Link>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <Link to="/workpage" className="dropdown-item">
+                    Works
+                  </Link>
+                  <div className="dropdown-divider"></div>
+                  <Link to="/blogpage" className="dropdown-item">
+                    Blog
+                  </Link>
+                </div>
+              </li>
+              <li className="nav-item">
+                <Link to="/help" className="nav-link">
+                  How we can help
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+        
+        {/* Font Awesome icon to toggle menu, visible only during media query */}
+        <FontAwesomeIcon icon={faBars} className="navbar-toggler" onClick={toggleMenu} />
+      </nav>
     </div>
   );
 };
